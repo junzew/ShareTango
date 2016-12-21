e0bdcd031e42e281a2a8d2cd6b4f759c0ea7b1c0
Junze Wu <junzew@hotmail.com>
2016-12-21 15:00:36 -0800

    Add Fragment layout stub

--- app/src/main/java/com/imran/wali/sharetango/UI/Fragments/AlbumFragment.java
+++ app/src/main/java/com/imran/wali/sharetango/UI/Fragments/AlbumFragment.java
@@ -1,7 +1,31 @@
 package com.imran.wali.sharetango.UI.Fragments;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.imran.wali.sharetango.R;
+
 /**
  * Created by wali on 07/04/16.
  */
 public class AlbumFragment extends PagerAdapterTabFragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getAlbums();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_list, container, false);
+    }
+
+    private void getAlbums() {
+
+    }
 }
--- app/src/main/java/com/imran/wali/sharetango/UI/Fragments/GenreFragment.java
+++ app/src/main/java/com/imran/wali/sharetango/UI/Fragments/GenreFragment.java
@@ -1,7 +1,21 @@
 package com.imran.wali.sharetango.UI.Fragments;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.imran.wali.sharetango.R;
+
 /**
  * Created by wali on 07/04/16.
  */
 public class GenreFragment extends PagerAdapterTabFragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_list, container, false);
+    }
 }
