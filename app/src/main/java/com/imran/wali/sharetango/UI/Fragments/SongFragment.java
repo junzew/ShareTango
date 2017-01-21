0cc2e6b5f79b5eda0f186f05c04651c6e6f4b56b
Junze Wu <junzew@hotmail.com>
2017-01-20 16:45:44 -0800

    Add pull to refresh library

--- app/src/main/java/com/imran/wali/sharetango/UI/Fragments/SongFragment.java
+++ app/src/main/java/com/imran/wali/sharetango/UI/Fragments/SongFragment.java
@@ -11,12 +11,14 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.Toast;
 
+import com.handmark.pulltorefresh.library.PullToRefreshBase;
 import com.imran.wali.sharetango.AudioManager.MusicData;
 import com.imran.wali.sharetango.AudioManager.PlaybackController;
 import com.imran.wali.sharetango.DashboardActivity;
 import com.imran.wali.sharetango.R;
 import com.imran.wali.sharetango.UI.Elements.IndexableListView.IndexableListAdapter;
 import com.imran.wali.sharetango.UI.Elements.IndexableListView.IndexableListView;
+import com.imran.wali.sharetango.UI.Elements.IndexableListView.PullToRefreshIndexableListView;
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.PermissionToken;
 import com.karumi.dexter.listener.PermissionDeniedResponse;
@@ -27,11 +29,12 @@ import com.karumi.dexter.listener.single.PermissionListener;
 /**
  * Created by wali on 07/04/16.
  */
-public class SongFragment extends PagerAdapterTabFragment {
+public class SongFragment extends PagerAdapterTabFragment implements PullToRefreshBase.OnRefreshListener {
 
     private DashboardActivity mContext;
     private IndexableListView listView;
     private IndexableListAdapter adapter;
+    private PullToRefreshIndexableListView pullToRefreshListView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -76,10 +79,15 @@ public class SongFragment extends PagerAdapterTabFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.songs_fragment, container, false);
-        listView =  (IndexableListView) view.findViewById(R.id.song_list);
+
+        pullToRefreshListView = (PullToRefreshIndexableListView) view.findViewById(R.id.pull_to_refresh_lv);
+        pullToRefreshListView.setOnRefreshListener(this);
+        listView = pullToRefreshListView.getRefreshableView();
+
+        listView.setFastScrollEnabled(true); // must come before setting adapter
         listView.setAdapter(adapter);
-        listView.setFastScrollEnabled(true);
         listView.setOnItemClickListener(songClickListener);
+
         return view;
     }
 
@@ -109,4 +117,18 @@ public class SongFragment extends PagerAdapterTabFragment {
                 .setIcon(android.R.drawable.ic_dialog_alert)
                 .show();
     }
+
+
+    @Override
+    public void onRefresh(PullToRefreshBase refreshView) {
+        Toast.makeText(mContext, "refreshing", Toast.LENGTH_SHORT).show();
+        pullToRefreshListView.postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                pullToRefreshListView.onRefreshComplete();
+            }
+        }, 1000);
+    }
+
 }
